c00816709bb306e305ebff89b8ca4e789c5b283d
Aakash Gupta <aakash.25b01011199@abes.ac.in>
2026-07-23 11:54:00 +0530

    initial commit

---
+++ Lab1/hello.js
@@ -0,0 +1,26 @@
+console.log("Hello")
+
+//write a fn to take two no. and return their sum
+
+// older method
+function add(a,b){
+    let sum = a+b;
+    return sum;
+
+}
+
+// console.log(add(2,2));
+
+//arrow fn
+const addd=(a,b)=>{
+    return a+b;
+
+}
+// console.log(addd(3,2))
+
+// adv. arrow fn
+// if we have only one line of statement then we will not write the return keyword 
+const add2=(a,b)=>a+b;  //a+b is returned by itself
+console.log(add2(3,2))
+
+
