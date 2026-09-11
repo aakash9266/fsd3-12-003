eb4cd1aad5a581c21433f7825a13e0337297bf91
Aakash Gupta <aakash.25b01011199@abes.ac.in>
2026-09-11 15:56:19 +0530

    lab4_updated

---
+++ lab4/users.js
@@ -0,0 +1,8 @@
+// we use in memory data base 
+let users=[
+    {id:1,name:'kash',mob:'98759xxxxx',email:'kash.example@exam.com'},
+    {id:2,name:'aakash',mob:'98759xxxxx',email:'aakash.example@exam.com'},
+]
+
+let nextId=3;
+export const getUser = ()=>users;
