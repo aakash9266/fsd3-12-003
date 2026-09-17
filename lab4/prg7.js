2a301a0d592ca2886c5955840513776b11a9c130
Aakash Gupta <aakash.25b01011199@abes.ac.in>
2026-09-17 12:22:02 +0530

    add user and get user by id created

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,14 +1,30 @@
 import http from "http";
-import { getUser } from "./users.js"
+import { addUser, getUsers } from "./users.js";
 
 const server = http.createServer((req, res) => {
-  if ((req.url === "/api/users" && req.method === "GET")) {
+  if (req.url === "/api/users" && req.method === "GET") {
     res.end(JSON.stringify(getUser()));
   } else if (req.url === "/api/users" && req.method === "POST") {
-    res.end(JSON.stringify({ msg: "add user" }));
-  } else if (req.url === "/api/users/1" && req.method === "GET") {
-    res.end(JSON.stringify({ msg: "single user with id 1" }));
-  } else if (req.url === "/api/users/1" && req.method === "PUT") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const user = JSON.parse(body);
+
+      const userCreated = addUser(user);
+      res.end(JSON.stringify({ msg: "user added", userCreated }));
+    });
+  } else if (req.url.startsWith("/api/users/1") && req.method === "GET") {
+
+    const userId=Number(req.url.split('/').pop())
+
+    res.end(JSON.stringify({ msg: `Showing details of user with id ${userId} 
+      ` }));
+
+  } 
+  else if (req.url === "/api/users/1" && req.method === "PUT") {
     res.end(JSON.stringify({ msg: "update user 1" }));
   } else if (req.url === "/api/users/1" && req.method === "DELETE") {
     res.end(JSON.stringify({ msg: "remove 1" }));
--- lab4/users.js
+++ lab4/users.js
@@ -1,8 +1,20 @@
-// we use in memory data base 
-let users=[
-    {id:1,name:'kash',mob:'98759xxxxx',email:'kash.example@exam.com'},
-    {id:2,name:'aakash',mob:'98759xxxxx',email:'aakash.example@exam.com'},
-]
+// we use in memory data base
+let users = [
+  { id: 1, name: "kash", mob: "98759xxxxx", email: "kash.example@exam.com" },
+  {
+    id: 2,
+    name: "aakash",
+    mob: "98759xxxxx",
+    email: "aakash.example@exam.com",
+  },
+];
 
-let nextId=3;
-export const getUser = ()=>users;
+let nextId = 3;
+export const getUsers = () => users;
+
+export const addUser = (user) => {
+  user.id = nextId++;
+  users.push(user);
+
+  return user;
+};
